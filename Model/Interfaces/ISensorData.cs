3b8a2c7161f1559ee5a0319065cf7ab2a3f06c0a
Bogdan Moldovean <bogdan.moldovean@live.com>
2020-03-16 16:40:18 +0200

    Added ISensorData fields. Mostly just checking that my email is set correctly

--- Model/Interfaces/ISensorData.cs
+++ Model/Interfaces/ISensorData.cs
@@ -1,7 +1,19 @@
+using System;
+
 namespace planthydra_api.Model.Interfaces
 {
     public interface ISensorData
     {
+        int Id { get; set; }
 
+        string? Name { get; set; }
+
+        int Duration { get; set; }
+
+        DateTime StartTime { get; set; }
+
+        string Schedule { get; set; }
+
+        IDevice Device { get; set; }
     }
 }
